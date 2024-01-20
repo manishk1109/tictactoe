31c21d131bf9e8255da6feb55c1271585dea791e
Manish <113422288+manishk1109@users.noreply.github.com>
2024-01-20 18:36:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let count = 0;
 let turn = 'X';
 let turnaudio = new Audio("ting.mp3");
 let gameoveraudio = new Audio("gameover.mp3");
@@ -32,20 +33,21 @@ const checkWin = () => {
     win.forEach(e => {
         if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText == boxtext[e[2]].innerText) && (boxtext[e[0]].innerText !== "") && (boxtext[e[1]].innerText !== "") && (boxtext[e[2]].innerText !== "")) {
             document.querySelector('.result').innerText = boxtext[e[0]].innerText + " won";
-            if(boxtext[e[0]].innerText === "0"){
+            count = 0;
+            if (boxtext[e[0]].innerText === "0") {
                 score0.innerHTML = Number(score0.innerHTML) + 1;
             }
-            if(boxtext[e[0]].innerText === "X"){
+            if (boxtext[e[0]].innerText === "X") {
                 scoreX.innerHTML = Number(scoreX.innerHTML) + 1;
             }
-           setTimeout(()=>{
-            document.querySelector('.winnerMsz').style.display = "block";
-            if(gameover){
-                gameoveraudio.play();
-            }
-           },1000);
+            setTimeout(() => {
+                document.querySelector('.winnerMsz').style.display = "block";
+                if (gameover) {
+                    gameoveraudio.play();
+                }
+            }, 1000);
             gameover = true;
-            
+
 
         }
     })
@@ -55,21 +57,23 @@ const checkWin = () => {
 
 
 let reset = document.querySelector(".reset");
-reset.addEventListener('click',()=>{
+reset.addEventListener('click', () => {
     let boxes = document.querySelectorAll(".box");
-    Array.from(boxes).forEach(box =>{
+    Array.from(boxes).forEach(box => {
         box.innerHTML = "";
         gameover = false;
+        count = 0;
     })
 })
 
 let start = document.querySelector(".playagain");
-start.addEventListener('click',()=>{
+start.addEventListener('click', () => {
     let boxes = document.querySelectorAll(".box");
-    Array.from(boxes).forEach(box =>{
+    Array.from(boxes).forEach(box => {
         box.innerHTML = "";
         document.querySelector('.winnerMsz').style.display = "none";
         gameover = false;
+        count = 0;
     })
 })
 
@@ -77,17 +81,32 @@ start.addEventListener('click',()=>{
 
 
 let boxes = document.querySelectorAll(".box");
-let count = 0
+
 Array.from(boxes).forEach(box => {
     box.addEventListener('click', () => {
         if (box.innerHTML === "" && !gameover) {
             box.innerHTML = turn;
             checkWin();
             changeTurn();
-            nextTurn.innerHTML = 'Turn for '+ turn ;
+            nextTurn.innerHTML = 'Turn for ' + turn;
             turnaudio.play();
+            console.log(count);
             count++
-            
+            if (count == 9 && !gameover) {
+                nextTurn.innerHTML = "Its a tie";
+                console.log(count);
+                count = 0;
+                setTimeout(() => {
+                    let boxes = document.querySelectorAll(".box");
+                    Array.from(boxes).forEach(box => {
+                        box.innerHTML = "";
+                        nextTurn.innerHTML = 'Turn for ' + turn;
+                        count = 0;
+                    })
+                }, 1000)
+
+            }
+
         }
     });
 });
